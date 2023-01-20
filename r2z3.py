c0b99c1019a260822fb37b095a1618aa45ed1f41
gams-nikolina <80062473+gams-nikolina@users.noreply.github.com>
2023-01-20 15:22:24 +0100

    Update r2z3.py

--- r2z3.py
+++ r2z3.py
@@ -1,5 +1,5 @@
 #3. Napiši program koji unosi korisničko ime te provjerava je li korisničko ime upisano. Ukoliko je upisano neka ispiše poruku : "Dobro došao, korisničko ime!", a ukoliko nije neka ispiše poruku: "Niste upisali korisničko ime."
-ime=input('Upiši korisničko ime:  ')
+ime=str(input('Upiši korisničko ime:  '))
 if ime=='':
     print("Niste upisali korisničko ime.")
 else:
